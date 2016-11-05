d5b63dc63f3870e1e1f3b19a4ab61f8ad4ea31c5
Alex Myasoedov <msoedov@gmail.com>
2016-11-05 12:43:33 -0400

    Build -> append

--- lib/ex_form.ex
+++ lib/ex_form.ex
@@ -4,7 +4,7 @@ defmodule ExForm do
         []
     end
 
-    defp build(state, kind, question, extras, opts) do
+    defp append(state, kind, question, extras, opts) do
         required = Keyword.get(opts, :required, false)
         description = Keyword.get(opts, :description, "")
         data = %{
@@ -18,7 +18,7 @@ defmodule ExForm do
 
     def multiple_choice(state, question, choices, opts \\ []) do
         extras = %{"choices": Enum.map(choices, fn c -> %{"label" => c} end)}
-        build(state, "multiple_choice", question = question, extras = extras, opts = opts)
+        append(state, "multiple_choice", question = question, extras = extras, opts = opts)
     end
 
     def short_text(state, question, max_characters \\ nil, opts \\ []) do
@@ -26,7 +26,7 @@ defmodule ExForm do
             nil -> %{}
             _ -> %{"max_characters": max_characters}
         end
-        build(state, "short_text", question = question, extras = extras, opts = opts)
+        append(state, "short_text", question = question, extras = extras, opts = opts)
     end
 
     def long_text(state, question, max_characters \\ nil, opts \\ []) do
@@ -34,18 +34,18 @@ defmodule ExForm do
             nil -> %{}
             _ -> %{"max_characters": max_characters}
         end
-        build(state, "long_text", question = question, extras = extras, opts = opts)
+        append(state, "long_text", question = question, extras = extras, opts = opts)
     end
 
     def yes_no(state, question, opts \\ []) do
-        build(state, "yes_no", question = question, extras = %{}, opts = opts)
+        append(state, "yes_no", question = question, extras = %{}, opts = opts)
     end
 
     def number(state, question, min_value \\ nil,  max_value \\ nil, opts \\ []) do
         extras = %{}
         extras = if min_value, do: Dict.put(extras, "min_value", min_value), else: extras
         extras = if max_value, do: Dict.put(extras, "max_value", max_value), else: extras
-        build(state, "number", question = question, extras = extras, opts = opts)
+        append(state, "number", question = question, extras = extras, opts = opts)
     end
 
     def rating(state, question, steps \\ 5,  shape \\ "star", opts \\ []) do
@@ -53,7 +53,7 @@ defmodule ExForm do
           "shape": shape,
           "steps": steps,
         }
-        build(state, "rating", question = question, extras = extras, opts = opts)
+        append(state, "rating", question = question, extras = extras, opts = opts)
     end
 
     def opinion_scale(state, question, left, center, right, steps \\ 11, start_at_one \\ false, opts \\ []) do
@@ -67,7 +67,7 @@ defmodule ExForm do
           "start_at_one": start_at_one
 
         }
-        build(state, "opinion_scale", question = question, extras = extras, opts = opts)
+        append(state, "opinion_scale", question = question, extras = extras, opts = opts)
     end
 
     def publish(fields, title, web_hook, tags \\ [:deafult]) do
